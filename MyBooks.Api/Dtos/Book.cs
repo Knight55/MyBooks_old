7392425f85667fb8f4520dad7254d97edc0a3ab8
Tóth Dániel <toth.dani92@hotmail.com>
2018-11-29 00:01:45 +0100

    Added dtos and mapper configuration using AutoMapper.

---
+++ MyBooks.Api/Dtos/Book.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using MyBooks.Bll.Entities;
+
+namespace MyBooks.Api.Dtos
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public Genre Genre { get; set; }
+
+        public List<Author> Authors { get; set; }
+    }
+}
---
+++ MyBooks.Api/Mapping/MapperConfig.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using AutoMapper;
+using MyBooks.Bll.Entities;
+
+namespace MyBooks.Api.Mapping
+{
+    public class MapperConfig
+    {
+        public static IMapper Configure()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AllowNullCollections = true;
+                cfg.CreateMap<Book, Dtos.Book>()
+                    .ForMember(dto => dto.Authors, opt => opt.Ignore())
+                    .AfterMap((b, dto, ctx) =>
+                    {
+                        dto.Authors = b.Writings.Select(w => ctx.Mapper.Map<Dtos.Author>(w.Author)).ToList();
+                    });
+                cfg.CreateMap<Dtos.Book, Book>();
+
+                cfg.CreateMap<Author, Dtos.Author>();
+                cfg.CreateMap<Dtos.Author, Author>();
+
+                // TODO: CreateMap for other entities and dtos as well
+            });
+
+            var mapper = config.CreateMapper();
+            return mapper;
+        }
+    }
+}
