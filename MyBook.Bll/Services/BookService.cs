67ae874dfa55e1be6d230b61cb1f49d858c2227e
Tóth Dániel <toth.dani92@hotmail.com>
2018-11-29 00:05:07 +0100

    Added service to CRUD books and added exception when an entity is not found.

---
+++ MyBook.Bll/Services/BookService.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using MyBooks.Bll.Context;
+using MyBooks.Bll.Entities;
+using MyBooks.Bll.Exceptions;
+
+namespace MyBooks.Bll.Services
+{
+    public class BookService : IBookService
+    {
+        private readonly MyBookContext _context;
+
+        public BookService(MyBookContext context)
+        {
+            _context = context;
+        }
+
+        public Book GetBook(int bookId)
+        {
+            return _context.Books
+                .Include(b => b.Editions)
+                .ThenInclude(e => e.Publisher)
+                .Include(b => b.Writings)
+                .ThenInclude(w => w.Author)
+                .SingleOrDefault(b => b.Id == bookId) ?? throw new EntityNotFoundException("Book not found.");
+        }
+
+        public IEnumerable<Book> GetBooks()
+        {
+            var books = _context.Books
+                .Include(b => b.Editions)
+                .ThenInclude(e => e.Publisher)
+                .Include(b => b.Writings)
+                .ThenInclude(w => w.Author)
+                .ToList();
+
+            return books;
+        }
+
+        public Book InsertBook(Book newBook)
+        {
+            _context.Books.Add(newBook);
+            _context.SaveChanges();
+            return newBook;
+        }
+
+        public void UpdateBook(int bookId, Book updatedBook)
+        {
+            updatedBook.Id = bookId;
+            var entry = _context.Attach(updatedBook);
+            entry.State = EntityState.Modified;
+
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                var book = _context.Books.AsNoTracking().SingleOrDefault(b => b.Id == bookId)
+                           ?? throw new EntityNotFoundException("Book not found.");
+
+                throw;
+            }
+        }
+
+        public void DeleteBook(int bookId)
+        {
+            _context.Books.Remove(new Book { Id = bookId });
+
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                var book = _context.Books.AsNoTracking().SingleOrDefault(b => b.Id == bookId)
+                           ?? throw new EntityNotFoundException("Book not found");
+
+                throw;
+            }
+        }
+    }
+}
---
+++ MyBook.Bll/Services/IBookService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using MyBooks.Bll.Entities;
+
+namespace MyBooks.Bll.Services
+{
+    public interface IBookService
+    {
+        Book GetBook(int bookId);
+        IEnumerable<Book> GetBooks();
+        Book InsertBook(Book newBook);
+        void UpdateBook(int bookId, Book updatedBook);
+        void DeleteBook(int bookId);
+    }
+}
