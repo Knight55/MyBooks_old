31cefa8ac48f026b396cb24db7114e4a8b318af5
Tóth Dániel <toth.dani92@hotmail.com>
2018-11-30 17:45:34 +0100

    Created service using Refit.

--- MyBooks.Client/Services/IMyBookApiService.cs
+++ MyBooks.Client/Services/IMyBookApiService.cs
@@ -1,7 +1,16 @@
-﻿namespace MyBooks.Client.Services
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using MyBooks.Client.Models;
+using Refit;
+
+namespace MyBooks.Client.Services
 {
     public interface IMyBookApiService
     {
-        
+        [Get("/api/Book/{id}")]
+        Task<Book> GetBookAsync(int id);
+
+        [Get("/api/Book")]
+        Task<List<Book>> GetBooksAsync();
     }
 }
