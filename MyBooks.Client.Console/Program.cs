103c8c6abd5ca2aa24b535864133dd42209120b2
Tóth Dániel <toth.dani92@hotmail.com>
2018-11-30 13:12:03 +0100

    Added a new net core console app to test the API.

---
+++ MyBooks.Client.Console/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace MyBooks.Client.Console
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            System.Console.WriteLine("Book id: ");
+            var id = System.Console.ReadLine();
+            await GetBookAsync(int.Parse(id));
+
+            System.Console.ReadKey();
+        }
+
+        private static async Task GetBookAsync(int id)
+        {
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync(new Uri($"http://localhost:5000/api/Book/{id}"));
+                if (response.IsSuccessStatusCode)
+                {
+                    var json = await response.Content.ReadAsStringAsync();
+                    System.Console.WriteLine(json);
+                }
+            }
+        }
+    }
+}
