b851d4995dffedddc6b134895e3eb5c9a85bbfeb
Tóth Dániel <toth.dani92@hotmail.com>
2018-12-01 17:57:15 +0100

    Added inital views for WPF.

--- MyBooks.Client.WPF/App.xaml.cs
+++ MyBooks.Client.WPF/App.xaml.cs
@@ -5,8 +5,13 @@ using System.Data;
 using System.Diagnostics;
 using System.Linq;
 using System.Net.Http;
+using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows;
+using MyBooks.Client.Services;
+using ReactiveUI;
+using Refit;
+using Splat;
 
 namespace MyBooks.Client.WPF
 {
@@ -15,9 +20,12 @@ namespace MyBooks.Client.WPF
     /// </summary>
     public partial class App : Application
     {
-        protected override void OnStartup(StartupEventArgs e)
+        public App()
         {
-            base.OnStartup(e);
+            // A helper method that will register all classes that derive off IViewFor
+            // into our dependency injection container. ReactiveUI uses Splat for it's
+            // dependency injection by default, but you can override this if you like.
+            Locator.CurrentMutable.RegisterViewsForViewModels(Assembly.GetCallingAssembly());
         }
     }
 }
---
+++ MyBooks.Client.WPF/Views/BookDetailsView.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Reactive.Disposables;
+using MyBooks.Client.ViewModels;
+using ReactiveUI;
+
+namespace MyBooks.Client.WPF.Views
+{
+    /// <summary>
+    /// Interaction logic for BookDetailsView.xaml
+    /// </summary>
+    public partial class BookDetailsView : ReactiveUserControl<BookDetailsViewModel>
+    {
+        public BookDetailsView()
+        {
+            InitializeComponent();
+
+            this.WhenActivated(disposableRegistration =>
+            {
+                this.OneWayBind(ViewModel,
+                        viewModel => viewModel.Title,
+                        view => view.titleTextBlock.Text)
+                    .DisposeWith(disposableRegistration);
+            });
+        }
+    }
+}
