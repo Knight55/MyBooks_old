7c8d21411429a0630bb46555dec769c8fa9d059b
Tóth Dániel <toth.dani92@hotmail.com>
2018-12-02 14:25:31 +0100

    Restructured code between UserControls and MainWindow.

--- MyBooks.Client/ViewModels/AppViewModel.cs
+++ MyBooks.Client/ViewModels/AppViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Reactive;
 using System.Reactive.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -14,53 +15,14 @@ using Refit;
 
 namespace MyBooks.Client.ViewModels
 {
-    public class AppViewModel : ReactiveObject
+    public class AppViewModel : ReactiveObject, IScreen
     {
-        private string _searchTerm;
-        public string SearchTerm
-        {
-            get => _searchTerm;
-            set => this.RaiseAndSetIfChanged(ref _searchTerm, value);
-        }
-
-        private readonly ObservableAsPropertyHelper<IEnumerable<Book>> _results;
-        public IEnumerable<Book> Results => _results.Value;
-
-        private readonly ObservableAsPropertyHelper<bool> _isAvailable;
-        public bool IsAvailable => _isAvailable.Value;
+        public RoutingState Router { get; }
 
         public AppViewModel()
         {
-            // TODO: Modify this
-            _results = this
-                .WhenAnyValue(x => x.SearchTerm)
-                .Throttle(TimeSpan.FromMilliseconds(500))
-                .Select(term => term?.Trim())
-                .DistinctUntilChanged()
-                .SelectMany(GetBooks)
-                .ObserveOn(RxApp.MainThreadScheduler)
-                .ToProperty(this, x => x.Results);
-
-            _isAvailable = this
-                .WhenAnyValue(x => x.Results)
-                .Select(results => results != null)
-                .ToProperty(this, x => x.IsAvailable);
-        }
-
-        private async Task<IEnumerable<Book>> GetBooks(string searchTerm, CancellationToken token)
-        {
-            var myBooksApiService = RestService.For<IMyBookApiService>("http://localhost:5000");
-
-            List<Book> books;
-            if (string.IsNullOrEmpty(searchTerm))
-            {
-                books = await myBooksApiService.GetBooksAsync().ConfigureAwait(false);
-            }
-            else
-            {
-                books = await myBooksApiService.SearchBooksAsync(searchTerm).ConfigureAwait(false);
-            }
-            return books;
+            Router = new RoutingState();
+            Router.Navigate.Execute(new BookSearchViewModel(this));
         }
     }
 }
--- MyBooks.Client/ViewModels/BookDetailsViewModel.cs
+++ MyBooks.Client/ViewModels/BookDetailsViewModel.cs
@@ -1,21 +1,32 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Reactive;
 using MyBooks.Client.Models;
 using ReactiveUI;
 
 namespace MyBooks.Client.ViewModels
 {
-    public class BookDetailsViewModel : ReactiveObject
+    public class BookDetailsViewModel : ReactiveObject, IRoutableViewModel
     {
+        public string UrlPathSegment => "bookDetails";
+        public IScreen HostScreen { get; }
+
+        public ReactiveCommand<Unit, Unit> GoBack { get; }
+
         private readonly Book _book;
-
-        public BookDetailsViewModel(Book book)
-        {
-            _book = book;
-        }
-
         public Uri CoverUrl => new Uri(_book.CoverUrl);
         public string Title => _book.Title;
-        public string Summary => _book.Summary;
+        public string Summary => _book.Summary ?? "No summary";
         public string Genre => _book.Genre;
+
+        public BookDetailsViewModel(IScreen hostScreen, Book book)
+        {
+            HostScreen = hostScreen;
+            _book = book;
+            GoBack = ReactiveCommand.Create(() =>
+            {
+                HostScreen.Router.NavigateBack.Execute();
+            });
+        }
     }
 }
