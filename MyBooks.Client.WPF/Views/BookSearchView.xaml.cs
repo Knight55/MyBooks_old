3cab55a71e8590ca934223d9fcb3359865e47994
Tóth Dániel <toth.dani92@hotmail.com>
2018-12-02 14:24:46 +0100

    Added new UserControl and ViewModel for searching books.

---
+++ MyBooks.Client.WPF/Views/BookSearchView.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reactive;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Windows.Controls;
+using MyBooks.Client.Models;
+using MyBooks.Client.ViewModels;
+using ReactiveUI;
+
+namespace MyBooks.Client.WPF.Views
+{
+    /// <summary>
+    /// Interaction logic for BookSearchView.xaml
+    /// </summary>
+    public partial class BookSearchView : ReactiveUserControl<BookSearchViewModel>
+    {
+        public BookSearchView()
+        {
+            InitializeComponent();
+
+            // We create our bindings here. These are the code behind bindings which allow 
+            // type safety. The bindings will only become active when the Window is being shown.
+            // We register our subscription in our disposableRegistration, this will cause 
+            // the binding subscription to become inactive when the Window is closed.
+            // The disposableRegistration is a CompositeDisposable which is a container of 
+            // other Disposables. We use the DisposeWith() extension method which simply adds 
+            // the subscription disposable to the CompositeDisposable.
+            this.WhenActivated(disposableRegistration =>
+            {
+                this.OneWayBind(ViewModel, vm => vm.Results, v => v.bookResultsListBox.ItemsSource)
+                    .DisposeWith(disposableRegistration);
+
+                this.Bind(ViewModel, vm => vm.SearchTerm, v => v.searchTextBox.Text)
+                    .DisposeWith(disposableRegistration);
+
+                IObservable<EventPattern<SelectionChangedEventArgs>> selectionChangedObservable =
+                    Observable
+                        .FromEventPattern<SelectionChangedEventArgs>(bookResultsListBox, "SelectionChanged");
+                selectionChangedObservable
+                    .Subscribe(evt =>
+                    {
+                        var listBox = (ListBox)evt.Sender;
+                        var book = (Book)listBox?.SelectedItem;
+                        if (book != null)
+                        {
+                            Debug.WriteLine($"Clicked on book: {book?.Title}");
+                            ViewModel.GoToBookDetails.Execute(book).Subscribe();
+                        }
+                    });
+            });
+        }
+    }
+}
---
+++ MyBooks.Client/ViewModels/BookSearchViewModel.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using MyBooks.Client.Models;
+using MyBooks.Client.Services;
+using ReactiveUI;
+using Refit;
+
+namespace MyBooks.Client.ViewModels
+{
+    public class BookSearchViewModel : ReactiveObject, IRoutableViewModel
+    {
+        public string UrlPathSegment => "bookSearch";
+        public IScreen HostScreen { get; }
+
+        private string _searchTerm;
+        public string SearchTerm
+        {
+            get => _searchTerm;
+            set => this.RaiseAndSetIfChanged(ref _searchTerm, value);
+        }
+
+        private readonly ObservableAsPropertyHelper<IEnumerable<Book>> _results;
+        public IEnumerable<Book> Results => _results.Value;
+
+        private readonly ObservableAsPropertyHelper<bool> _isAvailable;
+        public bool IsAvailable => _isAvailable.Value;
+
+        public ReactiveCommand<Book, Unit> GoToBookDetails { get; }
+
+        public BookSearchViewModel(IScreen hostScreen)
+        {
+            HostScreen = hostScreen;
+
+            GoToBookDetails = ReactiveCommand.Create<Book>(b =>
+            {
+                HostScreen.Router.Navigate.Execute(new BookDetailsViewModel(HostScreen, b)).Subscribe();
+            });
+
+            _results = this
+                .WhenAnyValue(x => x.SearchTerm)
+                .Throttle(TimeSpan.FromMilliseconds(700))
+                .Select(term => term?.Trim())
+                .DistinctUntilChanged()
+                .SelectMany(GetBooks)
+                .ObserveOn(RxApp.MainThreadScheduler)
+                .ToProperty(this, x => x.Results);
+
+            _isAvailable = this
+                .WhenAnyValue(x => x.Results)
+                .Select(results => results != null)
+                .ToProperty(this, x => x.IsAvailable);
+        }
+
+        /// <summary>
+        /// Searching books by their title. If no search term is given then
+        /// returns with every book available.
+        /// </summary>
+        /// <param name="searchTerm"></param>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        private async Task<IEnumerable<Book>> GetBooks(string searchTerm, CancellationToken token)
+        {
+            var myBooksApiService = RestService.For<IMyBookApiService>("http://localhost:5000");
+
+            List<Book> books;
+            if (string.IsNullOrEmpty(searchTerm))
+            {
+                books = await myBooksApiService.GetBooksAsync().ConfigureAwait(false);
+            }
+            else
+            {
+                books = await myBooksApiService.SearchBooksAsync(searchTerm).ConfigureAwait(false);
+            }
+            return books;
+        }
+    }
+}
