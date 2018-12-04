adb89c2ef07e3aa24b56ba2b7104f2a214d978e0
Tóth Dániel <toth.dani92@hotmail.com>
2018-12-04 09:40:23 +0100

    Add static reference to an AppBootStrapper instead of dependency injection here.

--- MyBooks.Client.WPF/App.xaml.cs
+++ MyBooks.Client.WPF/App.xaml.cs
@@ -9,6 +9,7 @@ using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows;
 using MyBooks.Client.Services;
+using MyBooks.Client.ViewModels;
 using ReactiveUI;
 using Refit;
 using Splat;
@@ -20,12 +21,11 @@ namespace MyBooks.Client.WPF
     /// </summary>
     public partial class App : Application
     {
+        public static AppBootStrapper BootStrapper;
+
         public App()
         {
-            // A helper method that will register all classes that derive off IViewFor
-            // into our dependency injection container. ReactiveUI uses Splat for it's
-            // dependency injection by default, but you can override this if you like.
-            Locator.CurrentMutable.RegisterViewsForViewModels(Assembly.GetCallingAssembly());
+            BootStrapper = new AppBootStrapper();
         }
     }
 }
