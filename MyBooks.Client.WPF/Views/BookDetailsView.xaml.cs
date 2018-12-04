dab480e3962863800c3b7286fd127058be4a1243
Tóth Dániel <toth.dani92@hotmail.com>
2018-12-04 09:36:32 +0100

    Add cover image navigation, authors, summary, goodreads url and genre to the UI and bindings.

--- MyBooks.Client.WPF/Views/BookDetailsView.xaml.cs
+++ MyBooks.Client.WPF/Views/BookDetailsView.xaml.cs
@@ -16,20 +16,31 @@ namespace MyBooks.Client.WPF.Views
 
             this.WhenActivated(disposableRegistration =>
             {
+                // Properties
                 this.OneWayBind(ViewModel, vm => vm.Title, v => v.titleRun.Text)
                     .DisposeWith(disposableRegistration);
 
                 this.OneWayBind(ViewModel, vm => vm.Genre, v => v.genreRun.Text)
                     .DisposeWith(disposableRegistration);
 
-                this.OneWayBind(ViewModel, vm => vm.CoverUrl, v => v.coverImage.Source,
-                        url => url == null ? null : new BitmapImage(url))
+                this.OneWayBind(ViewModel, vm => vm.Authors, v => v.authorsRun.Text)
+                    .DisposeWith(disposableRegistration);
+
+                this.OneWayBind(ViewModel, vm => vm.Rating, v => v.ratingRun.Text)
                     .DisposeWith(disposableRegistration);
 
                 this.OneWayBind(ViewModel, vm => vm.Summary, v => v.summaryTextBox.Text)
                     .DisposeWith(disposableRegistration);
 
+                this.OneWayBind(ViewModel, vm => vm.CoverUrl, v => v.coverImage.Source,
+                        url => url == null ? null : new BitmapImage(url))
+                    .DisposeWith(disposableRegistration);
+                
+                // Commands
                 this.BindCommand(ViewModel, vm => vm.GoBack, v => v.backButton)
+                    .DisposeWith(disposableRegistration);
+
+                this.BindCommand(ViewModel, vm => vm.OpenGoodreadsUrl, v => v.goodreadsLink)
                     .DisposeWith(disposableRegistration);
             });
         }
