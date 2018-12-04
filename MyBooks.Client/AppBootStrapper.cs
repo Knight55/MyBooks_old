6e7270a650044da1e7b07e5193d0add5881d9c3b
Tóth Dániel <toth.dani92@hotmail.com>
2018-12-04 09:35:11 +0100

    Add bootstrapper class which derives from IScreen and is the base of navigation. Dependency injection is set up here.

---
+++ MyBooks.Client/AppBootStrapper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Reflection;
+using MyBooks.Client.Services;
+using MyBooks.Client.ViewModels;
+using ReactiveUI;
+using Refit;
+using Splat;
+
+namespace MyBooks.Client
+{
+    public class AppBootStrapper : ReactiveObject, IScreen
+    {
+        public RoutingState Router { get; }
+
+        public AppBootStrapper()
+        {
+            Router = new RoutingState();
+
+            // Services
+            Locator.CurrentMutable.Register(() =>
+                RestService.For<IMyBookApiService>("http://localhost:5000"));
+
+            // ViewModels
+            Locator.CurrentMutable.RegisterLazySingleton(() => this, typeof(IScreen));
+            Locator.CurrentMutable.RegisterLazySingleton(() => new AppViewModel(
+                Locator.Current.GetService<IScreen>()));
+            Locator.CurrentMutable.Register(() => new BookSearchViewModel(
+                Locator.Current.GetService<IScreen>(),
+                Locator.Current.GetService<IMyBookApiService>()
+                ), typeof(BookSearchViewModel));
+            Locator.CurrentMutable.Register(() => new BookDetailsViewModel(
+                Locator.Current.GetService<IScreen>()
+                ), typeof(BookDetailsViewModel));
+
+            // Views
+            Locator.CurrentMutable.RegisterViewsForViewModels(Assembly.GetEntryAssembly());
+        }
+    }
+}
